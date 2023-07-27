ed59f43d0e0b1d6cae6ded0aea55b28b8ed4580d
Kinzerskiy <kinzzersky@gmail.com>
2023-07-28 00:28:34 +0300

    Initial Commit

---
+++ eva_camera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  eva_camera
+//
+//  Created by Mac Pro on 28.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
